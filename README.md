196c40243aaf220668178589a10d8961669c4ad3
valandro <lvalandrorocha@hotmail.com>
2018-10-16 07:46:43 -0300

    Initialize repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Data
+This project consists in a CRUD application.
+
+### Dependencies
+ - Gradle 4.3
+ - Java 10.0.2
+ - Lombok 1.8
---
+++ src/main/java/com/springdata/springdata/SpringdataApplication.java
@@ -0,0 +1,12 @@
+package com.springdata.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataApplication.class, args);
+	}
+}
